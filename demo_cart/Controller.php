897a5b7fe3e882fdd3d42a38884561841ca21cb7
Cory McDonald <cmcdonald@brave.com>
2020-03-30 16:23:29 -0500

    Rename id -> sku

--- demo_cart/Controller.php
+++ demo_cart/Controller.php
@@ -8,21 +8,21 @@ class Controller {
 
      public function invoke()
      {
-          $id = "910e2f59-4c3d-4a7e-959b-f73d68866ddd";
+          $sku = "BRAVE-12345";
           $amount = "8";
           $currency = "BAT";
           $description = "12 ounces of Coffee";
           $expiry = strtotime("+5 minutes");
 
-          $m_12oz = Sku::generateSKUToken($_ENV['SKU_SECRET'], $id, $amount, $currency, $description, $expiry);
+          $m_12oz = Sku::generateSKUToken($_ENV['SKU_SECRET'], $sku, $amount, $currency, $description, $expiry);
 
-          $id = "56a6189f-06e0-47b5-aebb-0eb17c009130";
+          $sku = "BRAVE-54321";
           $amount = "10";
           $currency = "BAT";
           $description = "1 pound of Coffee";
           $expiry = date(DateTime::RFC3339, strtotime("+5 minutes"));
 
-          $m_1lb = Sku::generateSKUToken($_ENV['SKU_SECRET'], $id, $amount, $currency, $description, $expiry);
+          $m_1lb = Sku::generateSKUToken($_ENV['SKU_SECRET'], $sku, $amount, $currency, $description, $expiry);
           include 'View.php';
      }
 }
